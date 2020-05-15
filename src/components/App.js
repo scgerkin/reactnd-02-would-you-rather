70c6141a7d1ad044771b7020adc2cda5965b6a55
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-14 23:02:11 -0400

    Navigation with address bar works as expected

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {MemoryRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import '../styles/App.css';
 import {NavContainer} from "./Navigation/NavContainer";
 import {Home} from "./Home/Home";
--- src/components/Navigation/NavContainer.js
+++ src/components/Navigation/NavContainer.js
@@ -1,19 +1,23 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from 'react-router-bootstrap';
 
-//fixme This actually navigates away from the page and causes a reload which
-// is not the behavior we want. This needs to be investigated further with
-// Bootstrap. It looks nice, though.
 export class NavContainer extends React.Component {
   render() {
     return (
         <Navbar bg={"dark"} variant={"dark"}>
           <Navbar.Brand>Would You Rather?</Navbar.Brand>
           <Nav className={"mr-auto"}>
-            <Nav.Link href={"/"}>Home</Nav.Link>
-            <Nav.Link href={"/questions"}>Questions</Nav.Link>
-            <Nav.Link href={"/leaderboard"}>Leaderboard</Nav.Link>
+            <LinkContainer to={"/"}>
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to={"/questions"}>
+              <Nav.Link>Questions</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to={"/leaderboard"}>
+              <Nav.Link>Leaderboard</Nav.Link>
+            </LinkContainer>
           </Nav>
           <Nav className={"ml-auto"}>
             <Nav.Link>Logout</Nav.Link>
