08345422aca645022ee3f6d8a1c0ec4f6feab9d9
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-14 23:24:35 -0400

    Add bootstrap dependency to package

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import {NavContainer} from "./Navigation/NavContainer";
 import {Home} from "./Home/Home";
 import {Leaderboard} from "./Leaderboard/Leaderboard";
 import {Questions} from "./Questions/Questions";
+import {Auth} from "./Auth/Auth";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route exact path={"/"} component={Home}/>
             <Route path={"/questions"} component={Questions}/>
             <Route path={"/leaderboard"} component={Leaderboard}/>
+            <Route path={"/auth"} component={Auth}/>
           </div>
         </div>
       </Router>
--- src/components/Navigation/NavContainer.js
+++ src/components/Navigation/NavContainer.js
@@ -1,26 +1,29 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar";
-import { LinkContainer } from 'react-router-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 export class NavContainer extends React.Component {
   render() {
     return (
         <Navbar bg={"dark"} variant={"dark"}>
-          <Navbar.Brand>Would You Rather?</Navbar.Brand>
+          <Navbar.Brand>Would You Rather...?</Navbar.Brand>
           <Nav className={"mr-auto"}>
-            <LinkContainer to={"/"}>
+            <LinkContainer exact to={"/"}>
               <Nav.Link>Home</Nav.Link>
             </LinkContainer>
             <LinkContainer to={"/questions"}>
               <Nav.Link>Questions</Nav.Link>
             </LinkContainer>
             <LinkContainer to={"/leaderboard"}>
-              <Nav.Link>Leaderboard</Nav.Link>
+              <Nav.Link>Leader Board</Nav.Link>
             </LinkContainer>
           </Nav>
+          {/*Fixme: when clicked, the previous nav item is still highlighted*/}
           <Nav className={"ml-auto"}>
-            <Nav.Link>Logout</Nav.Link>
+            <LinkContainer to={"/auth"}>
+              <Nav.Link>Logout</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar>
     )
