305fbc244648be904257ad650d84eb8c22270c5b
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-15 22:11:08 -0400

    Navbar good enough.

--- src/components/Navigation/NavContainer.js
+++ src/components/Navigation/NavContainer.js
@@ -2,12 +2,27 @@ import React from "react";
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar";
 import {LinkContainer} from 'react-router-bootstrap';
+import {connect} from "react-redux";
+import FigureImage from "react-bootstrap/FigureImage";
+import Button from "react-bootstrap/Button";
+
 
 class NavContainer extends React.Component {
+
+  onLogout() {
+    console.log("logged out");
+  }
+
   render() {
+    const {user} = this.props;
+    const {name, avatarURL} = user ? user : {name: "", avatarURL: ""};
     return (
-        <Navbar bg={"dark"} variant={"dark"}>
-          <Navbar.Brand>Would You Rather...?</Navbar.Brand>
+        <Navbar bg={"dark"} variant={"dark"} expand={"lg"}>
+          <Navbar.Brand>
+            Would you rather...?
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+          <Navbar.Collapse>
           <Nav className={"mr-auto"}>
             <LinkContainer exact to={"/"}>
               <Nav.Link>Home</Nav.Link>
@@ -22,15 +37,25 @@ class NavContainer extends React.Component {
               <Nav.Link>Leader Board</Nav.Link>
             </LinkContainer>
           </Nav>
-          {/*Fixme: when clicked, the previous nav item is still highlighted*/}
-          <Nav className={"ml-auto"}>
-            <LinkContainer to={"/auth"}>
-              <Nav.Link>Logout</Nav.Link>
-            </LinkContainer>
-          </Nav>
+            <Navbar.Text>{name}&nbsp;</Navbar.Text>
+            <FigureImage
+                src={avatarURL}
+                width={30}
+                height={30}
+                roundedCircle/>
+            <div>&nbsp;&nbsp;</div>
+          <Button variant="outline-light" onClick={this.onLogout}>Logout</Button>
+          </Navbar.Collapse>
         </Navbar>
     )
   }
 }
 
-export default NavContainer;
+function mapStateToProps({authedUser, users}) {
+  return {
+    user: authedUser ? users[authedUser] : null
+  }
+}
+
+
+export default connect(mapStateToProps)(NavContainer);
