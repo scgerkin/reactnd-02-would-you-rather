7b459e52e88c2ce04e6c32b004a5c9b31f07b466
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-16 18:30:09 -0400

    Remove deprecated vote card on question parent

--- src/components/Questions/Question.js
+++ src/components/Questions/Question.js
@@ -1,9 +1,6 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import {handleVote} from "../../actions/questions";
 import Container from "react-bootstrap/Container";
 import Vote from "./Vote";
 
@@ -14,22 +11,8 @@ import Vote from "./Vote";
 //  It can determine which one should be displayed and give the appropriate
 //  information based on the component to display
 class Question extends Component {
-  state = {
-    option: ""
-  }
-
-  _onOptionChange(option) {
-    const {dispatch, authedUser} = this.props;
-
-    this.setState({
-      option: option
-    });
-    dispatch(handleVote(authedUser, this.props.id, option));
-  }
-
   render() {
     const {question} = this.props;
-    const {option} = this.state;
 
     return (
         <Container>
@@ -38,25 +21,6 @@ class Question extends Component {
               author={question.author} // todo, this needs to be retrieved from users state
               authorAvatar={"http://placekitten.com/150/150"} //todo
           />
-
-
-        <Card>
-          <Card.Header>{question.author} asks...</Card.Header>
-          <Card.Title>Would you rather...</Card.Title>
-          <Card.Text>
-              <Button
-                  onClick={this._onOptionChange.bind(this, "optionOne")}
-                  active={option === "optionOne"}
-                  variant={option === "optionOne" ? "success":"secondary"}
-              >{question.optionOne}</Button>Votes:{question.oneVotes}
-              <br/>
-              <Button
-                  onClick={this._onOptionChange.bind(this, "optionTwo")}
-                  active={option === "optionTwo"}
-                  variant={option === "optionTwo" ? "success":"secondary"}
-              >{question.optionTwo}</Button>Votes:{question.twoVotes}
-          </Card.Text>
-        </Card>
         </Container>
     );
   }
