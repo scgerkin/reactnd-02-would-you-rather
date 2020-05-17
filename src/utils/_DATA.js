1d3605ded399d24db3677991b4d26e65fd91f4f3
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-17 16:37:06 -0400

    give everyone unique placeholder avatars

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: "http://placekitten.com/150/150",
+    avatarURL: "http://placekitten.com/149/149",
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: "http://placekitten.com/150/150",
+    avatarURL: "http://placekitten.com/151/151",
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
